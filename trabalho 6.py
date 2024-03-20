b1ef26b69fb224f835b8b5b77df262e2454d6d37
fabriciovivas <160743554+fabriciovivas@users.noreply.github.com>
2024-03-20 09:40:45 -0300

    Add files via upload

---
+++ trabalho 6.py
@@ -0,0 +1,44 @@
+print("Fabricio")
+
+numero = 7
+nome = "Fabricio"
+booleano = True
+
+a = 15
+b = 10
+
+soma = a + b
+subtração = a - b
+
+
+numeros = [10, 20, 30, 40]
+frase = "Glorioso de Futebol e Regatas."
+palavra = "Tiquinho"
+
+
+media = sum(numeros) / len(numeros)
+print("Média aritmética dos números:", media)
+
+
+numero_quadrado = numeros[0] ** 2
+print("Quadrado do primeiro número:", numero_quadrado)
+
+
+dobro_numero = numeros[1] * 2
+print("Dobro do segundo número:", dobro_numero)
+
+
+quantidade_letras = len(palavra)
+print("Quantidade de letras na palavra:", quantidade_letras)
+
+
+quantidade_espacos = frase.count(" ")
+print("Quantidade de espaços em branco na frase:", quantidade_espacos)
+
+
+primeiro_maior = numeros[0] > numeros[1]
+print("O primeiro número é maior que o segundo?", primeiro_maior)
+
+
+maior_numero = max(numeros)
+print("O maior número é:", maior_numero)
